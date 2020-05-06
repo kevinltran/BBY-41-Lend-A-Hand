6cecf74aafe81f2fec2c09f057165f799a32c39b
Leon Wu <21leonwu@gmail.com>
2020-05-06 11:25:34 -0700

    added request.js

--- scripts/viewHelpees.js
+++ scripts/viewHelpees.js
@@ -1,22 +1,23 @@
 function viewHelpees() {
   firebase.auth().onAuthStateChanged(function (user) {
-    // if (user) {
-    let info = document.getElementById("info");
-    let helpees = db.collection("users").where("role", "==", "helpee");
-    helpees.get().then(function (list) {
-      list.forEach(function (doc) {
-        let name = doc.data().name;
-        let link = document.createElement("a");
-        let br = document.createElement("br");
-        link.innerHTML = name;
-        link.setAttribute("href", "");
-        info.append(link);
-        info.append(br);
+    if (user) {
+      let info = document.getElementById("info");
+      let helpees = db.collection("users").where("role", "==", "helpee");
+      helpees.get().then(function (list) {
+        list.forEach(function (doc) {
+          let name = doc.data().name;
+          let link = document.createElement("a");
+          let br = document.createElement("br");
+          link.innerHTML = name;
+          link.setAttribute("href", "404.html");
+          link.setAttribute("onclick", "request()");
+          info.append(link);
+          info.append(br);
+        });
       });
-    });
-    // }
-    // else {
-    //   console.log("not logged in");
-    // }
+    }
+    else {
+      console.log("not logged in");
+    }
   });
 }
