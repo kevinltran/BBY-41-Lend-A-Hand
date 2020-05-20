71fde3ad1f11a9c0d4c98c0f1c00389ce0756425
Leon Wu <21leonwu@gmail.com>
2020-05-20 14:24:37 -0700

    test

--- scripts/createhelpee.js
+++ scripts/createhelpee.js
@@ -8,45 +8,45 @@ let imageRef;
 
 //Gets the values from the fields when the user clicks confirm.
 document.getElementById("clicked").onclick = function () {
-    homeAddress = document.getElementById("addressInput").value;
-    city = document.getElementById("city").value;
-    postCode = document.getElementById("postCode").value;
-    phoneNumber = document.getElementById("phone").value;
-    bio = document.getElementById("userBio").value;
+  homeAddress = document.getElementById("addressInput").value;
+  city = document.getElementById("city").value;
+  postCode = document.getElementById("postCode").value;
+  phoneNumber = document.getElementById("phone").value;
+  bio = document.getElementById("userBio").value;
 
-    console.log(imageRef);
-    console.log(phoneNumber);
-    console.log(homeAddress);
-    console.log(city);
-    console.log(postCode);
-    console.log(bio);
-    write(phoneNumber, homeAddress, city, postCode, imageRef);
+  console.log(imageRef);
+  console.log(phoneNumber);
+  console.log(homeAddress);
+  console.log(city);
+  console.log(postCode);
+  console.log(bio);
+  write(phoneNumber, homeAddress, city, postCode, imageRef);
 };
 
 // This function creates a new doc in our "Helper" collection in our Database.
 function write(phoneNumber, homeAddress, city, postCode, imageRef) {
-    firebase.auth().onAuthStateChanged(function (user) {
-        if (user) {
-            db.collection("users").doc(user.uid).update({
-                address: homeAddress,
-                phone: phoneNumber,
-                city: city,
-                postalCode: postCode,
-                description: bio,
-                role: "helpee",
-                image: imageRef
-            })
-        }
-    })
-       
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      db.collection("users").doc(user.uid).update({
+        address: homeAddress,
+        phone: phoneNumber,
+        city: city,
+        postalCode: postCode,
+        description: bio,
+        role: "helpee",
+        image: imageRef
+      })
+    }
+  })
 
-    //puts a delay on the redirect so the data can be written into our database.
-    setTimeout(function () { redirect(); }, 1000);
+
+  //puts a delay on the redirect so the data can be written into our database.
+  setTimeout(function () { redirect(); }, 1000);
 }
 
 //redirects the user to the helper main page.
 function redirect() {
-    window.location.href = "helpeeProfile.html"
+  window.location.href = "helpeeProfile.html"
 }
 
 var uploader = document.getElementById("uploader");
@@ -65,19 +65,21 @@ fileButton.addEventListener('change', function (e) {
   imageRef = file.name;
   // Update Bar
   task.on('state_changed',
-      function progress(snapshot) {
-          var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          uploader.value = percentage;
-      }, 
-      
-      function error(err) {
+    function progress(snapshot) {
+      var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      uploader.value = percentage;
+    },
 
-      },
-     
-      function complete() {
-          console.log("image saved in database");
-      }
+    function error(err) {
+
+    },
+
+    function complete() {
+      console.log("image saved in database");
+    }
 
   );
 
 });
+
+
