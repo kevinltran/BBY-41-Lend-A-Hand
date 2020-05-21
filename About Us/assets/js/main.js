189688ad62da1b22e3c23082a1a4fc41fd778a98
Robert Wilson <robertwilson875@gmail.com>
2020-05-21 11:17:54 -0700

    cleaned up code and commented

--- About Us/assets/js/main.js
+++ About Us/assets/js/main.js
@@ -4,7 +4,7 @@
  * Author: BootstrapMade.com
  * License: https://bootstrapmade.com/license/
  */
-!(function ($) {
+(function ($) {
   "use strict";
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
